c1bbf23578cb7246c3c935f2deb28df93807e30a
François Parmentier <francois.parmentier@gmail.com>
2018-11-29 15:37:45 +0100

    :hammer: Improve convertJsonLdToNQuads
    Lint + error

--- src/convertJsonLdToNQuads.js
+++ src/convertJsonLdToNQuads.js
@@ -3,19 +3,20 @@ import jsonld from 'jsonld';
 export default function convertJsonLdToNQuads(data, feed) {
     if (this.isLast()) {
         feed.close();
+    } else {
+        jsonld.toRDF(
+            data,
+            {
+                format: 'application/nquads',
+            },
+            (err, nquads) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.error('toRDF: ', err);
+                    throw new Error(err);
+                }
+                feed.send(nquads);
+            },
+        );
     }
-    jsonld.toRDF(
-        data,
-        {
-            format: 'application/nquads',
-        },
-        (err, nquads) => {
-            if (err) {
-                // eslint-disable-next-line no-console
-                console.error('toRDF: ', err);
-                return feed.end();
-            }
-            return feed.send(nquads);
-        },
-    );
 }

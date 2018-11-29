d7939ad2bff48cf80578898b12cdab422cd1f236
François Parmentier <francois.parmentier@gmail.com>
2018-11-29 16:14:56 +0100

    :sparkles: Add convertToAtom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import flattenPatch from './flatten-patch';
 import objects2columns from './objects2columns';
 import convertJsonLdToNQuads from './convertJsonLdToNQuads';
+import convertToAtom from './convertToAtom';
 
 export default {
     flattenPatch,
     objects2columns,
     convertJsonLdToNQuads,
+    convertToAtom,
     // aliases
     fixFlatten: flattenPatch.flattenPatch,
 };

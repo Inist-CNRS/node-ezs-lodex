b448b885b8595f3535a76426b5b93d4ed3b8b5c4
François Parmentier <francois.parmentier@gmail.com>
2018-11-29 16:20:32 +0100

    :hammer: Improve test

--- test/convertToAtom.spec.js
+++ test/convertToAtom.spec.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import path from 'path';
+import from from 'from';
 import { Feed } from 'feed';
 import ezs from 'ezs';
 import statements from '../src';
@@ -16,39 +15,24 @@ describe('convertToAtom', () => {
             image:
                 'https://user-images.githubusercontent.com/7420853/30152932-1794db3c-93b5-11e7-98ab-a7f28d0061cb.png',
         });
-        const mongoData = fs.createReadStream(
-            path.resolve(__dirname, './fixture.data.mongo.json'),
-        );
-        mongoData
+        from([{}])
             .pipe(ezs('convertToAtom', { fields: [], config: {}, atomFeed }))
             .pipe(ezs((input) => {
                 try {
                     const lines = input.split('\n');
-                    expect(lines[0]).toBe(
-                        '<?xml version="1.0" encoding="utf-8"?>',
-                    );
-                    expect(lines[1]).toBe(
-                        '<feed xmlns="http://www.w3.org/2005/Atom">',
-                    );
+                    expect(lines[0]).toBe('<?xml version="1.0" encoding="utf-8"?>');
+                    expect(lines[1]).toBe('<feed xmlns="http://www.w3.org/2005/Atom">');
                     expect(lines[2]).toBe('    <id>id</id>');
-                    expect(lines[3]).toBe(
-                        '    <title>title</title>',
-                    );
-                    expect(lines[5]).toBe(
-                        '    <generator>Lodex</generator>',
-                    );
-                    expect(lines[6]).toBe(
-                        '    <link rel="alternate" href="link"/>',
-                    );
-                    expect(lines[7]).toBe(
-                        '    <logo>https://user-images.githubusercontent.com/7420853/30152932-1794db3c-93b5-11e7-98ab-a7f28d0061cb.png</logo>',
-                    );
+                    expect(lines[3]).toBe('    <title>title</title>');
+                    expect(lines[5]).toBe('    <generator>Lodex</generator>');
+                    expect(lines[6]).toBe('    <link rel="alternate" href="link"/>');
+                    expect(lines[7]).toBe('    <logo>https://user-images.githubusercontent.com/7420853/30152932-1794db3c-93b5-11e7-98ab-a7f28d0061cb.png</logo>');
                     expect(lines[8]).toBe('</feed>');
                     done();
                 } catch (e) {
                     done(e);
                 }
-            }));
-        mongoData.on('error', done);
+            }))
+            .on('error', done);
     });
 });

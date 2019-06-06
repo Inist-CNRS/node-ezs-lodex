19e61120045866e8e996506e1c37e164e2e0df5d
François Parmentier <francois.parmentier@gmail.com>
2019-06-06 18:19:56 +0200

    :heavy_check_mark: Make keyMapping tests pass

---
+++ src/keyMapping.js
@@ -0,0 +1,41 @@
+import zipObject from 'lodash.zipobject';
+
+/**
+ * Take an object and map its keys to the one in mapping parameter.
+ *
+ * @example <caption>Input:</caption>
+ * [{
+ *   "dFgH": "Value",
+ *   "AaAa": "Value 2"
+ * }]
+ *
+ * @example <caption>EZS:</caption>
+ * [keyMapping]
+ * from = dFgH
+ * to = Title
+ * from = AaAa
+ * to = Description
+ *
+ * @example <caption>Output</caption>
+ * [{
+ *   "Title": "Value",
+ *   "Description": "Value 2"
+ * }]
+ * @export
+ */
+export default function keyMapping(data, feed) {
+    const from = this.getParam('from', []);
+    const to = this.getParam('to', []);
+    const froms = Array.isArray(from) ? from : [from];
+    const tos = Array.isArray(to) ? to : [to];
+    const mapping = zipObject(froms, tos);
+
+    if (this.isLast()) {
+        return feed.close();
+    }
+    const res = Object
+        .keys(data)
+        .reduce((o, key) => ({ ...o, [mapping[key]]: data[key] }), {});
+    feed.write(res);
+    return feed.end();
+}

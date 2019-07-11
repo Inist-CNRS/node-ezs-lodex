3a32c279c14a4aabb2f6a7599d41e23af3d0d384
François Parmentier <francois.parmentier@gmail.com>
2019-07-11 18:29:28 +0200

    :white_check_mark: Update toTurtle test

--- test/convertNQuadsToTurtle.spec.js
+++ test/convertNQuadsToTurtle.spec.js
@@ -7,6 +7,7 @@ ezs.use(statements);
 
 describe('convertNQuadsToTurtle', () => {
     it('should convert N-Quads to turtle', (done) => {
+        let res = '';
         from([[
             '<http://uri/janedoe> <http://schema.org/jobTitle> "Professor" .',
             '<http://uri/janedoe> <http://schema.org/name> "Jane Doe" .',
@@ -14,20 +15,24 @@ describe('convertNQuadsToTurtle', () => {
             '<http://uri/janedoe> <http://schema.org/url> <http://www.janedoe.com> .',
             '<http://uri/janedoe> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .',
         ].join('\n')])
-            .pipe(ezs('convertNQuadsToTurtle'))
+            .pipe(ezs('parseNQuads'))
+            .pipe(ezs('writeTurtle'))
             .on('data', (data) => {
-                expect(data).toEqual([
-                    '@prefix schema: <http://schema.org/> .',
+                res += data;
+            })
+            .on('end', () => {
+                expect(res).toEqual([
+                    '@prefix schema: <http://schema.org/>.',
                     '',
-                    '<http://uri/janedoe>',
-                    '  schema:jobTitle "Professor" ;',
-                    '  schema:name "Jane Doe" ;',
-                    '  schema:telephone "(425) 123-4567" ;',
-                    '  schema:url <http://www.janedoe.com> ;',
-                    '  a schema:Person .',
+                    '<http://uri/janedoe> schema:jobTitle "Professor";',
+                    '    schema:name "Jane Doe";',
+                    '    schema:telephone "(425) 123-4567";',
+                    '    schema:url <http://www.janedoe.com>;',
+                    '    a schema:Person.',
                     '',
                 ].join('\n'));
                 done();
-            });
+            })
+            .on('error', done);
     });
 });

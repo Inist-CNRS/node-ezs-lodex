fabe74ae1b18f321bd80526226a0b010b3b02e75
François Parmentier <francois.parmentier@gmail.com>
2019-07-12 14:30:13 +0200

    :books: Document writeTurtle

--- src/writeTurtle.js
+++ src/writeTurtle.js
@@ -7,6 +7,50 @@ const uriPrefixes = Object.values(defaultPrefixes);
 /**
  * Take quad or prefixes object and return turtle string.
  *
+ * @example <caption>Input:</caption>
+ * [{
+ *    quad: {
+ *      subject: { id: 'http://uri/janedoe' },
+ *      predicate: { id: 'http://schema.org/jobTitle' },
+ *      object: { id: '"Professor"' }
+ *    }
+ *  }, {
+ *      quad: {
+ *      subject: { id: 'http://uri/janedoe' },
+ *      predicate: { id: 'http://schema.org/name' },
+ *      object: { id: '"Jane Doe"' }
+ *    }
+ *  }, {
+ *      quad: Quad {
+ *      subject: { id: 'http://uri/janedoe' },
+ *      predicate: { id: 'http://schema.org/telephone' },
+ *      object: { id: '"(425) 123-4567"' }
+ *      }
+ *  }, {
+ *      quad: Quad {
+ *      subject: { id: 'http://uri/janedoe' },
+ *      predicate: { id: 'http://schema.org/url' },
+ *      object: { id: 'http://www.janedoe.com' }
+ *      }
+ *  }, {
+ *      quad: Quad {
+ *      subject: { id: 'http://uri/janedoe' },
+ *      predicate: { id: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' },
+ *      object: { id: 'http://schema.org/Person' }
+ *      }
+ *  }, { prefixes: {} }
+ *  ]
+ *
+ * @example <caption>Output:</caption>
+ * '@prefix schema: <http://schema.org/>.\n'
+ * '@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n'
+ * '\n'
+ * '<http://uri/janedoe> schema:jobTitle "Professor";\n'
+ * '    schema:name "Jane Doe";\n'
+ * '    schema:telephone "(425) 123-4567";\n'
+ * '    schema:url <http://www.janedoe.com>;\n'
+ * '    a schema:Person.\n"'
+ *
  * @export
  * @return {String} turtle
  * @name writeTurtle
